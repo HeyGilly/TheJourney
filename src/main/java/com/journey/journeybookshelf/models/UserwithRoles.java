aa98e8570cae2833283fa23e0f0146bd17dacffe
Yasmine Rico <ricolyasmine@gmail.com>
2022-06-14 16:18:40 -0500

    fix UserWithRoles

--- src/main/java/com/journey/journeybookshelf/models/UserwithRoles.java
+++ src/main/java/com/journey/journeybookshelf/models/UserwithRoles.java
@@ -6,9 +6,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-public class UserwithRoles extends User implements UserDetails {
+public class UserWithRoles extends User implements UserDetails {
 
-    public UserwithRoles(User user) {
+    public UserWithRoles(User user) {
         super(user);  // Call the copy constructor defined in User
     }
 
