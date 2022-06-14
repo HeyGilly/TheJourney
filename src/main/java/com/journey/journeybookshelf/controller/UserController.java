1b19be106e5185254cfbcf19bccd0490a88f74b3
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-06-14 16:08:13 -0500

    added password form, not complete

--- src/main/java/com/journey/journeybookshelf/controller/UserController.java
+++ src/main/java/com/journey/journeybookshelf/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.journey.journeybookshelf.controller;
 
 
+import com.journey.journeybookshelf.models.Review;
 import com.journey.journeybookshelf.models.User;
 import com.journey.journeybookshelf.repository.ReviewRepository;
 import com.journey.journeybookshelf.repository.UserRepository;
@@ -65,12 +66,12 @@ public class UserController {
     @PostMapping("/profile/{username}")
     public String updateProfile(@ModelAttribute User userModel){
         User user = userDao.findByUsername(userModel.getUsername());
-        String hash = passwordEncoder.encode(userModel.getPassword());
+//        String hash = passwordEncoder.encode(userModel.getPassword());
         user.setFirstName(userModel.getFirstName());
         user.setLastName(userModel.getLastName());
         user.setEmail(userModel.getEmail());
         user.setUsername(userModel.getUsername());
-        user.setPassword(hash);
+//        user.setPassword(hash);
         user.setBio(userModel.getBio());
         user.setCity(userModel.getCity());
         user.setState(userModel.getState());
@@ -78,9 +79,41 @@ public class UserController {
         return "redirect:/profile/{username}";
     }
 
-    @PostMapping("/profile/delete")
-    public String deleteProfile(@RequestParam(name= "deleteProfile") long id) {
-        userDao.deleteById(id);
-        return "redirect:/register";
+//    @PostMapping("/profile/delete")
+//    public String deleteProfile(@RequestParam(name= "deleteProfile") long id, Model model) {
+////        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+////        model.addAttribute("user",user);
+////        long userId = user.getId();
+//        userDao.deleteById(id);
+//        return "redirect:/register";
+//    }
+
+    @GetMapping("/changePassword")
+    public String passwordForm(Model model){
+//        model.addAttribute("review", new Review());
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user",user);
+        return "users/changePassword";
     }
+
+    @PostMapping("/changePassword")
+    public String changePassword(@ModelAttribute User userModel){
+        // This is what was added to get it to work
+        // needed to save the user with the userDoa
+        User user = userDao.findByUsername(userModel.getUsername());
+        String hash = passwordEncoder.encode(userModel.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/profile/{username}";
+    }
+
+//    @PostMapping("/changePassword")
+//    public String updatePassword(Model model){
+//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        model.addAttribute("user",user);
+//        String hash = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(hash);
+//        userDao.save(user);
+//        return "redirect:/profile/{username}";
+//    }
 }
