a58884a9e2ae3be18d46ce845ccb3e1c42561946
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-06-21 15:12:55 -0500

    added code to not allow duplicate usernames to be created and display error message upon attempt

--- src/main/java/com/journey/journeybookshelf/controller/UserController.java
+++ src/main/java/com/journey/journeybookshelf/controller/UserController.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.naming.Binding;
 
 @Controller
 public class UserController {
@@ -32,9 +35,19 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String saveUser(@ModelAttribute User user){
+    public String saveUser(@ModelAttribute User user, BindingResult result){
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
+
+        User existing = userDao.findByUsername(user.getUsername());
+
+        if (existing != null){
+            result.rejectValue("username",null,"This username already exists");
+        }
+
+        if (result.hasErrors()){
+            return "users/register";
+        }
 
         userDao.save(user);
         return "redirect:/login";
